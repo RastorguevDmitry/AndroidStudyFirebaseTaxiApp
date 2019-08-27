79b65b133378602230971d04e208d65b0bfb672c
rdi81 <rdi81@yandex.ru>
2019-08-27 17:24:38 +0300

    validation

---
+++ app/src/main/java/com/RDI/androidstudyfirebasetaxiapp/ChoseModeActivity.java
@@ -0,0 +1,26 @@
+package com.RDI.androidstudyfirebasetaxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.RDI.androidstudyfirebasetaxiapp.R;
+
+public class ChoseModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose_mode);
+    }
+
+    public void goToPassengerSignIn(View view) {
+        startActivity(new Intent(ChoseModeActivity.this, PassengerSignInActivity.class));
+    }
+
+    public void goToDriverSignIn(View view) {
+        startActivity(new Intent(ChoseModeActivity.this, DrivgerSignInActivity.class));
+    }
+}
--- app/src/main/java/com/RDI/androidstudyfirebasetaxiapp/PassengerSignInActivity.java
+++ app/src/main/java/com/RDI/androidstudyfirebasetaxiapp/PassengerSignInActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PassengerSignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_passenger_sign_in);
     }
 }
